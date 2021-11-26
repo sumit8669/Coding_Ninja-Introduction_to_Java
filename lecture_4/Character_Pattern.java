acde0eb0e272d0139e149b35c98705df3a807053
sumit8669 <sumit2628r@gmail.com>
2021-11-26 10:37:09 +0530

    update Character Pattern

--- lecture_4/Character_Pattern.java
+++ lecture_4/Character_Pattern.java
@@ -7,16 +7,14 @@ public class Character_Pattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
         characterPattern(n);
     }
 
     private static void characterPattern(int input) {
 
         for (int i = 1; i <=input ; i++) {
+            // NEED ADDITION OF CURRENT INDEX WITH CHARACTER
             char startingChar = (char)('A'+i-1);
-            // need axillary to store count
-
             for (int j = 1; j <=i ; j++) {
                 System.out.print(startingChar);
                 startingChar++;
