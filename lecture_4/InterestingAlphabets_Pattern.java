36e21ae92a1a9acda23de88dd5633616229f74e1
sumit8669 <sumit2628r@gmail.com>
2021-11-26 10:38:41 +0530

    update Interesting Alphabet Pattern

---
+++ lecture_4/InterestingAlphabets_Pattern.java
@@ -0,0 +1,25 @@
+package com.ninja.lecture_4;
+
+import java.util.Scanner;
+
+public class InterestingAlphabets_Pattern {
+
+    private static void interestingAlphabet(int input) {
+        for (int i =1 ; i <=input ; i++) {
+            // NEED SUBTRACTION FROM INPUT WITH CURRENT INDEX
+            char alpha =(char)('A'+input-i);
+            for(int j = 1; j <= i; j++){
+                System.out.print(alpha);
+                alpha++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        interestingAlphabet(n);
+    }
+
+    
+}
