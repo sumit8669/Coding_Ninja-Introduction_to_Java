93cbd0b4b32edf7efde990120cf23651a38ca4c2
sumit8669 <sumit2628r@gmail.com>
2021-11-26 08:31:28 +0530

    update alpha Pattern

---
+++ lecture_4/Alpha_Pattern.java
@@ -0,0 +1,21 @@
+package com.ninja.lecture_4;
+
+import java.util.Scanner;
+
+public class Alpha_Pattern {
+    
+    private static void alphaPattern(int input){
+        for (int i = 1; i <=input ; i++) {
+            for (int j =1; j <=i ; j++) {
+                System.out.print((char) ('A'+i-1));
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        alphaPattern(n);
+    }
+}
